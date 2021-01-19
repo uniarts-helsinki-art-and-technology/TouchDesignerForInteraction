edefae5e057a3259b3b43639611b9498c379836b
Roberto Fusco Pugliese <pugliesr@gmail.com>
2021-01-19 21:56:11 +0200

    j

---
+++ NeoPixel_controlledFromTouchDesigner/NeoPixel_controlledFromTouchDesigner.ino
@@ -0,0 +1,59 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// Released under the GPLv3 license to match the rest of the
+// Adafruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 12 // Popular NeoPixel ring size
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+const int numOfBytes = 36;
+char inputBuffer[numOfBytes];
+
+void setup() {
+  Serial.begin(115200);
+
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.clear();
+}
+
+void loop() {
+
+
+  if (Serial.available() > 0)
+  {
+
+    Serial.readBytes(inputBuffer, numOfBytes);
+    //pixels.clear(); // Set all pixel colors to 'off'
+ 
+  for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+   pixels.setPixelColor(i, pixels.Color(inputBuffer[3 * i], inputBuffer[3 * i + 1], inputBuffer[3 * i + 2]));
+    //pixels.setPixelColor(i, pixels.Color(200, 100, 50));
+
+    pixels.show();   // Send the updated pixel colors to the hardware.
+  }
+ }
+
+}
